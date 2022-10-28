13589a422bc3f66c4dce3a7623ac97819901ff62
Damian <bck1990@gmail.com>
2022-10-28 17:14:20 +0800

    added custom_auth for custom user model, and urls, views barebone

--- custom_auth/apps.py
+++ custom_auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class VisitsConfig(AppConfig):
+class CustomAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'visits'
+    name = 'custom_auth'
