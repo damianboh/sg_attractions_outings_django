9b7ba5006c2b7e60f4fc328628021e288a2a950a
Damian <bck1990@gmail.com>
2022-10-29 16:30:18 +0800

    changed login to use email, display email fields of users when using admin panel

--- custom_auth/admin.py
+++ custom_auth/admin.py
@@ -1,6 +1,38 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from custom_auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
-admin.site.register(User, UserAdmin) # to edit custom User model
+# custom user admin to include email fields instead of username fields
+class CustomUserAdmin(UserAdmin): 
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+admin.site.register(User, CustomUserAdmin)
