121785022a59d61e725fc5d6628c01a88666253b
Damian <bck1990@gmail.com>
2022-10-30 10:42:50 +0800

    added nav links

--- attractions/admin.py
+++ attractions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from attractions.models.attractions import SearchTerm, Tag, Attraction
+from attractions.models.outings import Outing, OutingInvitation, Comment
 
-# Register your models here.
+admin.site.register(SearchTerm)
+admin.site.register(Tag)
+admin.site.register(Attraction)
+admin.site.register(Outing)
+admin.site.register(OutingInvitation)
+admin.site.register(Comment)
