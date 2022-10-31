eec20f79932b7e82e1b743e2125d3853a1db1bd4
Damian <bck1990@gmail.com>
2022-10-31 12:16:09 +0800

    added outing form to create outings, to put in attraction details page

--- attractions/forms.py
+++ attractions/forms.py
@@ -1,2 +1,15 @@
 from socket import fromshare
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from .models.outings import Outing
+
+class OutingForm(forms.ModelForm):
+    class Meta:
+        model = Outing
+        fields = ["start_time"]
+
+    def __init__(self, *args, **kwargs):
+        super(OutingForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit("submit", "Create"))
