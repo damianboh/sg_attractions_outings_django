30a090efa0c17cc7914aa40c9da177f18691e55b
Damian <bck1990@gmail.com>
2022-10-31 10:41:30 +0800

    added save and remove functions

--- attractions/views.py
+++ attractions/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from attractions.models.attractions import Attraction
 from .models import Profile
 from .tourism_hub_integrated import search_and_save, fill_attraction_details
+from django.contrib import messages
 
 @login_required
 def search_attractions(request):
@@ -38,6 +39,26 @@ def search_attractions(request):
 def attraction_detail(request, uuid):
     attraction = get_object_or_404(Attraction, uuid=uuid)
     fill_attraction_details(attraction)
+
+    context = {"page_group": "search", "attraction": attraction}
+
+    return render(request, "attractions/single_attraction.html", context)
+
+@login_required
+def save_attraction(request, uuid):
+    attraction = get_object_or_404(Attraction, uuid=uuid)
+    attraction.saved_by.add(request.user.profile)
+    messages.add_message(request, messages.INFO, 'Attraction added to saved list.')
+
+    context = {"page_group": "search", "attraction": attraction}
+
+    return render(request, "attractions/single_attraction.html", context)
+
+@login_required
+def remove_attraction(request, uuid):
+    attraction = get_object_or_404(Attraction, uuid=uuid)
+    attraction.saved_by.remove(request.user.profile)
+    messages.add_message(request, messages.INFO, 'Attraction removed from saved list.')
 
     context = {"page_group": "search", "attraction": attraction}
 
