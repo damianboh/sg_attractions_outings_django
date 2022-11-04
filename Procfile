dcf8e02c27be315ce247cb7dc9b417bf1bc0e507
Damian <bck1990@gmail.com>
2022-11-04 12:13:09 +0800

    config for railway deployment

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: python manage.py migrate && gunicorn sg_attractions_outings.wsgi 
+web: python manage.py migrate 
+web: unicorn sg_attractions_outings.wsgi 
 worker: celery -A sg_attractions_outings worker -B --loglevel=info --scheduler django_celery_beat.schedulers:DatabaseScheduler
--- sg_attractions_outings/settings.py
+++ sg_attractions_outings/settings.py
@@ -24,9 +24,6 @@ class Dev(Configuration):
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = 'django-insecure-s!&a3ve&b9t_@t&%#n+6&&8kwv8ly6-$y)2y*($w=km^+_kvrp'
-
     # SECURITY WARNING: don't run with debug turned on in production!
     DEBUG = values.BooleanValue(True)
 
